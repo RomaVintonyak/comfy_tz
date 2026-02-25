4b2a9b7ab8a49ae42ebc4fcaa75a938acdddd9f3
Roma Vintonyak <19roman23@gmail.com>
2026-02-25 16:43:23 +0200

    create desktop version

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,18 @@
-/*jQuery(document).ready(function () {
-  "use script";
- 
-  var btn = $("._callback");
-  btn.on("click", function(){
-    alert("Its work");
-    prompt();
-  });
-});*/
 /*modal window*/
 const btnModal = document.querySelector('._modalBtn');
 const modalWindow = document.querySelector('._modalWindow');
 const modalClose = document.querySelector('._closeModal');
-btnModal.addEventListener('click', function(){
-    modalWindow.classList.add('modal__open');
+btnModal.addEventListener('click', function () {
+  modalWindow.classList.add('modal__open');
 });
-modalClose.addEventListener('click', function(){
+modalClose.addEventListener('click', function () {
   modalWindow.classList.remove('modal__open');
 });
-document.querySelector('.modal__mask').addEventListener('click', function(){
+document.querySelector('.modal__mask').addEventListener('click', function () {
   modalWindow.classList.remove('modal__open');
 });
-document.querySelector('.modal__mask--body').addEventListener('click', function(event){
+document.querySelector('.modal__mask--body').addEventListener('click', function (event) {
   event.stopPropagation();
 });
+
+
